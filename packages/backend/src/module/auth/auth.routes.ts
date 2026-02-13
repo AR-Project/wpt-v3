231f267c56935e642055593547edf0dc7eedbc6a
AR-Project <alvenresta@gmail.com>
2026-02-13 16:02:40 +0700

    be/fix/accessing admin via endpoint now will throw

--- packages/backend/src/module/auth/auth.routes.ts
+++ packages/backend/src/module/auth/auth.routes.ts
@@ -1,11 +1,12 @@
 import { auth, type PublicType } from '@lib/auth'
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 
 
 export const authRoute = new Hono<{ Variables: PublicType }>({
   strict: false,
 })
-  .all("/admin/*", async (c) => c.json({ message: "not Found" }, 404))
+  .all("/admin/*", async (c) => { throw new HTTPException(404) })
   .on(['POST', 'GET'], '/*', (c) => {
     return auth.handler(c.req.raw)
   })
