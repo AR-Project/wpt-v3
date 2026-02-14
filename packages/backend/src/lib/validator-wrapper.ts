0558da4b3e3bb73482dff39c4af66650944efdd8
AR-Project <alvenresta@gmail.com>
2026-02-15 00:37:12 +0700

    be/feat/zod-validator throw pretty string

---
+++ packages/backend/src/lib/validator-wrapper.ts
@@ -0,0 +1,20 @@
+// file: validator-wrapper.ts
+import { z, type ZodSchema } from "zod";
+import type { ValidationTargets } from "hono";
+import { zValidator as zv } from "@hono/zod-validator";
+import { HTTPException } from "hono/http-exception";
+
+export const zValidator = <
+	T extends ZodSchema,
+	Target extends keyof ValidationTargets,
+>(
+	target: Target,
+	schema: T,
+) =>
+	zv(target, schema, (result, _c) => {
+		if (!result.success) {
+			throw new HTTPException(400, {
+				message: z.prettifyError(result.error),
+			});
+		}
+	});
