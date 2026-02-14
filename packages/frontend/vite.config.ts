272603a10fd3df387f65a3a3197318a55a3848a7
AR-Project <alvenresta@gmail.com>
2026-02-14 23:36:19 +0700

    chore/delete base url for compability with ts7

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,40 +1,47 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    devtools(),
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 3000,
-    proxy: {
-      // Setup the proxy for the backend
-      "/api/": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-    fs: {
-      // Allow searching for files in the entire 'packages' directory
-      // or simply use '..' to allow the immediate parent
-      allow: ['..'],
-    },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-})
+	plugins: [
+		devtools(),
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		viteReact(),
+		tailwindcss(),
+	],
+	optimizeDeps: {
+		exclude: ["@wpt/backend"],
+	},
+	server: {
+		port: 3000,
+		proxy: {
+			// Setup the proxy for the backend
+			"/api/": {
+				target: "http://localhost:8000",
+				changeOrigin: true,
+			},
+		},
+		fs: {
+			// Allow searching for files in the entire 'packages' directory
+			// or simply use '..' to allow the immediate parent
+			allow: [".."],
+		},
+		watch: {
+			// Watch backend changes so frontend auto-reloads
+			ignored: ["!**/packages/backend/**"],
+		},
+	},
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
