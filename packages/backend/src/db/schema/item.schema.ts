768eda6dee37aadd15fd5f932cd9e2b544d98955
AR-Project <alvenresta@gmail.com>
2026-02-16 17:42:05 +0700

    be/fix/item user id is not unique

--- packages/backend/src/db/schema/item.schema.ts
+++ packages/backend/src/db/schema/item.schema.ts
@@ -6,54 +6,58 @@ import { relations } from "drizzle-orm";
 export type CreateItemDbPayload = typeof item.$inferInsert;
 
 export const item = sqliteTable(
-  "item",
-  (t) => ({
-    id: t.text("id").primaryKey().unique().notNull(),
-    userIdParent: t.text("user_id_parent")
-      .notNull()
-      .references(() => user.id, {
-        onDelete: "cascade",
-      }),
-    userIdCreator: t.text("user_id_creator")
-      .notNull()
-      .references(() => user.id, {
-        onDelete: "cascade",
-      }),
-    name: t.text("name").notNull(),
-    categoryId: t.text("category_id")
-      .notNull()
-      .references(() => category.id, {
-        onDelete: "set null",
-      }),
-    sortOrder: t.integer("sort_order"),
-    createdAt: t.integer("created_at", { mode: "timestamp_ms" })
-      .$defaultFn(() => new Date())
-      .notNull(),
-    modifiedAt: t.integer("last_modified_at", { mode: "timestamp_ms" })
-      .$onUpdateFn(() => new Date())
-      .notNull(),
-  })
-  ,
-  (table) => [
-    index("item_name_idx").on(table.name),
-    uniqueIndex("item_owner_idx").on(table.userIdParent),
-    uniqueIndex("item_creator_idx").on(table.userIdCreator),
-  ]
+	"item",
+	(t) => ({
+		id: t.text("id").primaryKey().unique().notNull(),
+		userIdParent: t
+			.text("user_id_parent")
+			.notNull()
+			.references(() => user.id, {
+				onDelete: "cascade",
+			}),
+		userIdCreator: t
+			.text("user_id_creator")
+			.notNull()
+			.references(() => user.id, {
+				onDelete: "cascade",
+			}),
+		name: t.text("name").notNull(),
+		categoryId: t
+			.text("category_id")
+			.notNull()
+			.references(() => category.id, {
+				onDelete: "set null",
+			}),
+		sortOrder: t.integer("sort_order"),
+		createdAt: t
+			.integer("created_at", { mode: "timestamp_ms" })
+			.$defaultFn(() => new Date())
+			.notNull(),
+		modifiedAt: t
+			.integer("last_modified_at", { mode: "timestamp_ms" })
+			.$onUpdateFn(() => new Date())
+			.notNull(),
+	}),
+	(table) => [
+		index("item_name_idx").on(table.name),
+		index("item_owner_idx").on(table.userIdParent),
+		index("item_creator_idx").on(table.userIdCreator),
+	],
 );
 
 export const itemRelations = relations(item, ({ one }) => ({
-  userParent: one(user, {
-    fields: [item.userIdParent],
-    references: [user.id],
-    relationName: "parent",
-  }),
-  userCreator: one(user, {
-    fields: [item.userIdCreator],
-    references: [user.id],
-    relationName: "creator",
-  }),
-  category: one(category, {
-    fields: [item.categoryId],
-    references: [category.id],
-  }),
+	userParent: one(user, {
+		fields: [item.userIdParent],
+		references: [user.id],
+		relationName: "parent",
+	}),
+	userCreator: one(user, {
+		fields: [item.userIdCreator],
+		references: [user.id],
+		relationName: "creator",
+	}),
+	category: one(category, {
+		fields: [item.categoryId],
+		references: [category.id],
+	}),
 }));
