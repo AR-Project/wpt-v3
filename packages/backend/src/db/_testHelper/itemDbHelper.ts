420ff5aad9d1df454eb764ed9026784e80f3fd06
AR-Project <alvenresta@gmail.com>
2026-02-16 18:53:27 +0700

    be/feat/improve readibility for clean method

--- packages/backend/src/db/_testHelper/itemDbHelper.ts
+++ packages/backend/src/db/_testHelper/itemDbHelper.ts
@@ -2,38 +2,41 @@ import { eq, or } from "drizzle-orm";
 import { db } from "@db/index";
 import { item, type CreateItemDbPayload } from "../schema/item.schema";
 
-
 type CleanOption = {
-  userId?: string | null
-  itemId?: string | null
-}
+	userId?: string | null;
+	itemId?: string | null;
+};
 
 export const itemTbHelper = {
-  add: async (payload: CreateItemDbPayload) => {
-    await db.insert(item).values(payload).returning({ id: item.id, name: item.name })
-  },
-  find: async (userId: string) => {
-    return await db.query.item.findMany({
-      where: (item, { eq, or }) =>
-        or(
-          eq(item.userIdCreator, userId),
-          eq(item.userIdParent, userId)
-        )
-    })
-  },
-  findAll: async () => await db.query.item.findMany(),
-  clean: async (option?: CleanOption) => {
-    const byUserFilter = option?.userId
-      ? or(
-        eq(item.userIdCreator, option.userId),
-        eq(item.userIdParent, option.userId),
-      )
-      : undefined
-    const byIdFilter = option?.itemId ? eq(item.id, option.itemId) : undefined
+	add: async (payload: CreateItemDbPayload) => {
+		await db
+			.insert(item)
+			.values(payload)
+			.returning({ id: item.id, name: item.name });
+	},
+	find: async (userId: string) => {
+		return await db.query.item.findMany({
+			where: (item, { eq, or }) =>
+				or(eq(item.userIdCreator, userId), eq(item.userIdParent, userId)),
+		});
+	},
+	findById: async (itemId: string) => {
+		return await db.query.item.findMany({
+			where: (item, { eq }) => eq(item.id, itemId),
+		});
+	},
+	findAll: async () => await db.query.item.findMany(),
+	clean: async (option?: CleanOption) => {
+		const filter = () => {
+			if (!option) return undefined;
+			if (option.itemId) return eq(item.id, option.itemId);
+			if (option.userId)
+				return or(
+					eq(item.userIdCreator, option.userId),
+					eq(item.userIdParent, option.userId),
+				);
+		};
 
-    await db
-      .delete(item)
-      .where(or(byUserFilter, byIdFilter)
-      )
-  }
-}
+		await db.delete(item).where(filter());
+	},
+};
