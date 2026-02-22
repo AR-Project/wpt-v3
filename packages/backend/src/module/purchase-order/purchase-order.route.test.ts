a13bb04410adbbc358f754182d696838875ca5c8
AR-Project <alvenresta@gmail.com>
2026-02-22 16:35:19 +0700

    be/feat/improve purchase-order test

--- packages/backend/src/module/purchase-order/purchase-order.route.test.ts
+++ packages/backend/src/module/purchase-order/purchase-order.route.test.ts
@@ -14,6 +14,7 @@ describe("purchase-order route", () => {
 	let currentUserId: string | null = "";
 	let cookie: string = "";
 	let defaultCategoryId: string = "";
+	const productIds = ["po_product_1", "po_product_2", "po_product_3"];
 
 	beforeAll(async () => {
 		const testUser = {
@@ -26,6 +27,26 @@ describe("purchase-order route", () => {
 		cookie = userData.cookie;
 		currentUserId = userData.id;
 		defaultCategoryId = userData.defaultCategoryId;
+
+		await Promise.all([
+			...productIds.map((id, index) =>
+				productTbHelper.add({
+					id,
+					name: `po-product-${index + 1}`,
+					userIdParent: currentUserId!,
+					userIdCreator: currentUserId!,
+					categoryId: defaultCategoryId,
+					sortOrder: index,
+				}),
+			),
+
+			vendorTbHelper.add({
+				id: "vendor_po_test",
+				name: "vendor_po_test",
+				userIdParent: currentUserId!,
+				userIdCreator: currentUserId!,
+			}),
+		]);
 	});
 
 	afterAll(async () => {
@@ -41,27 +62,6 @@ describe("purchase-order route", () => {
 	describe("POST endpoint", () => {
 		test.serial("should success", async () => {
 			// prepare
-			const productIds = ["po_product_1", "po_product_2", "po_product_3"];
-			await Promise.all([
-				...productIds.map((id, index) =>
-					productTbHelper.add({
-						id,
-						name: `po-product-${index + 1}`,
-						userIdParent: currentUserId!,
-						userIdCreator: currentUserId!,
-						categoryId: defaultCategoryId,
-						sortOrder: index,
-					}),
-				),
-
-				vendorTbHelper.add({
-					id: "vendor_po_test",
-					name: "vendor_po_test",
-					userIdParent: currentUserId!,
-					userIdCreator: currentUserId!,
-				}),
-			]);
-
 			const payload: CreatePurchaseOrderPayload = {
 				vendorId: "vendor_po_test",
 				totalCost: 50000,
@@ -95,16 +95,83 @@ describe("purchase-order route", () => {
 			});
 
 			// scoped clean up
-			await Promise.all([
-				...productIds.map((id) => productTbHelper.clean({ productId: id })),
-				vendorTbHelper.clean({ vendorId: "vendor_po_test" }),
-			]);
 			await purchaseOrderTbHelper.clean({ purchaseOrderId: json.data });
 
 			// assert
 			expect(res.status).toBe(201);
 			expect(createdPO.length).toBe(1);
 			expect(createdPO[0]?.purchaseItem.length).toBe(2);
+		});
+		test.serial("should fail when vendor is invalid", async () => {
+			// prepare
+			const payload: CreatePurchaseOrderPayload = {
+				vendorId: "vendor_invalid",
+				totalCost: 50000,
+				orderedAt: new Date(2026, 0, 1, 10, 10),
+				purchaseItems: [
+					{
+						productId: "po_product_1",
+						quantity: 4,
+						costPrice: 5000,
+					},
+					{
+						productId: "po_product_2",
+						quantity: 2,
+						costPrice: 15000,
+					},
+				],
+			};
+
+			// action
+			const res = await app.request("/api/purchase-order", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Cookie: cookie,
+				},
+				body: JSON.stringify(payload),
+			});
+			const json = (await res.json()) as { message: string };
+
+			// assert
+			expect(res.status).toBe(400);
+			expect(json.message).toBe("vendor ID invalid");
+		});
+
+		test.serial("should fail when some product is invalid", async () => {
+			// prepare
+			const payload: CreatePurchaseOrderPayload = {
+				vendorId: "vendor_po_test",
+				totalCost: 50000,
+				orderedAt: new Date(2026, 0, 1, 10, 10),
+				purchaseItems: [
+					{
+						productId: "po_product_1_invalid",
+						quantity: 4,
+						costPrice: 5000,
+					},
+					{
+						productId: "po_product_2",
+						quantity: 2,
+						costPrice: 15000,
+					},
+				],
+			};
+
+			// action
+			const res = await app.request("/api/purchase-order", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Cookie: cookie,
+				},
+				body: JSON.stringify(payload),
+			});
+			const json = (await res.json()) as { message: string };
+
+			// assert
+			expect(res.status).toBe(400);
+			expect(json.message).toBe("(some) product(s) ID invalid");
 		});
 	});
 });
