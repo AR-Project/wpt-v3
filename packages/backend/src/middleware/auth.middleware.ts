1fa30dd40316692a2b98d7f2fe5a14ecac9ca911
AR-Project <alvenresta@gmail.com>
2026-02-24 15:38:16 +0700

    be/feat/adding message on 401
    be/chore/biome got aggresive

--- packages/backend/src/middleware/auth.middleware.ts
+++ packages/backend/src/middleware/auth.middleware.ts
@@ -1,32 +1,39 @@
-import { auth, sanitizeUser, type ProtectedType, type PublicType } from '@/lib/auth';
-import { createMiddleware } from 'hono/factory'
-import { HTTPException } from 'hono/http-exception';
+import {
+	auth,
+	sanitizeUser,
+	type ProtectedType,
+	type PublicType,
+} from "@/lib/auth";
+import { createMiddleware } from "hono/factory";
+import { HTTPException } from "hono/http-exception";
 
 /**
  * @deprecated
  */
-export const authPublicMiddleware = createMiddleware<{ Variables: PublicType }>(async (c, next) => {
-  const session = await auth.api
-    .getSession({ headers: c.req.raw.headers });
+export const authPublicMiddleware = createMiddleware<{ Variables: PublicType }>(
+	async (c, next) => {
+		const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
-  if (!session) {
-    c.set("user", null);
-    c.set("session", null);
-    await next();
-    return;
-  }
+		if (!session) {
+			c.set("user", null);
+			c.set("session", null);
+			await next();
+			return;
+		}
 
-  c.set("user", session.user);
-  c.set("session", session.session);
-  await next()
-})
+		c.set("user", session.user);
+		c.set("session", session.session);
+		await next();
+	},
+);
 
-export const authProtectedMiddleware = createMiddleware<{ Variables: ProtectedType }>(async (c, next) => {
-  const session = await auth.api
-    .getSession({ headers: c.req.raw.headers });
-  if (!session) throw new HTTPException(401)
+export const authProtectedMiddleware = createMiddleware<{
+	Variables: ProtectedType;
+}>(async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+	if (!session) throw new HTTPException(401, { message: "unauthorized" });
 
-  c.set("user", sanitizeUser(session.user));
-  c.set("session", session.session);
-  await next()
-})
+	c.set("user", sanitizeUser(session.user));
+	c.set("session", session.session);
+	await next();
+});
