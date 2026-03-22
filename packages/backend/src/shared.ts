e76e578b3722435d939bd016d4cc7fb2df74ab0b
AR-Project <alvenresta@gmail.com>
2026-03-23 00:41:17 +0700

    be/feat: export schema on shared

--- packages/backend/src/shared.ts
+++ packages/backend/src/shared.ts
@@ -9,6 +9,11 @@ export type AuthTypeUser = AuthUserType;
 
 // Category schema
 export * from "@module/category/category.schema";
-
-// Item Schema
-export * as productSchema from "@/module/product/product.schema";
+export * as imageSchema from "@module/image/image.schema";
+export * as productSchema from "@module/product/product.schema";
+export * as profileSchema from "@module/profile/profile.schema";
+export * as purchaseOrderSchema from "@module/purchase-order/purchase-order.schema";
+export * as purchaseItemSchema from "@module/purchase-order/purchase-item/purchase-item.schema";
+export * as purchasePlanSchema from "@module/purchase-plan/purchase-plan.schema";
+export * as purchasePlanItemSchema from "@module/purchase-plan/purchase-plan-item/purchase-plan-item.schema";
+export * as vendorSchema from "@module/vendor/vendor.schema";
